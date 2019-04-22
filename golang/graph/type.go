33cdf3b862b3894d6d0810506dd0bcba076626b8
Qiang Li <qiang4.li@ge.com>
2019-04-22 12:12:35 -0700

    graph - adjacency matrix

--- golang/graph/type.go
+++ golang/graph/type.go
@@ -1,5 +1,11 @@
 package graph
 
+// https://en.wikipedia.org/wiki/Graph_(abstract_data_type)
+// AdjacencyList
+// AdjacencyMatrix
+// IncidenceMatrix
+//
+
 // simplistic set implementation
 type set []int
 
@@ -19,8 +25,11 @@ type Graph interface {
 	Neighbors(x int) []int
 	AddEdge(x, y int)
 	RemoveEdge(x, y int)
+	AddVertex(x int)
+	RemoveVertex(x int)
 }
 
+//
 type EdgeList [][2]int
 
 func (r EdgeList) Adjacent(x, y int) bool {
@@ -45,13 +54,9 @@ func (r EdgeList) Neighbors(x int) []int {
 	return []int(*s)
 }
 
-func (r *EdgeList) AddVertex(x int) {
-	//
-}
+func (r *EdgeList) AddVertex(x int) {}
 
-func (r *EdgeList) RemoveVertex(x int) {
-	//
-}
+func (r *EdgeList) RemoveVertex(x int) {}
 
 func (r *EdgeList) AddEdge(x, y int) {
 	if r.Adjacent(x, y) {
@@ -70,7 +75,52 @@ func (r *EdgeList) RemoveEdge(x, y int) {
 	}
 }
 
+// AdjacencyMatrix is a two-dimensional matrix, in which
+// the rows represent source vertices and columns represent destination vertices.
+// Data on edges and vertices must be stored externally.
 type AdjacencyMatrix [][]int
 
+func NewAdjacencyMatrix(n int) AdjacencyMatrix {
+	matrix := make([][]int, n)
+	for i := 0; i < n; i++ {
+		matrix[i] = make([]int, n)
+	}
+	return matrix
+}
+
+func (r AdjacencyMatrix) Adjacent(x, y int) bool {
+	return r[x][y] == 1 || r[y][x] == 1
+}
+
+func (r AdjacencyMatrix) Neighbors(x int) []int {
+	s := new(set)
+	for i, v := range r[x] {
+		if v == 1 {
+			s.add(i)
+		}
+	}
+	return []int(*s)
+}
+
+func (r *AdjacencyMatrix) AddEdge(x, y int) {
+	(*r)[x][y] = 1
+	(*r)[y][x] = 1
+}
+
+func (r *AdjacencyMatrix) RemoveEdge(x, y int) {
+	(*r)[x][y] = 0
+	(*r)[y][x] = 0
+}
+
+func (r *AdjacencyMatrix) AddVertex(x int)    {}
+func (r *AdjacencyMatrix) RemoveVertex(x int) {}
+
+// https://en.wikipedia.org/wiki/Adjacency_list
+// Vertices are stored as records or objects, and every vertex stores a list of adjacent vertices
 type AdjacencyList struct {
+}
+
+type IncidenceMatrix [][]int
+
+type IncidenceList struct {
 }
--- golang/graph/type_test.go
+++ golang/graph/type_test.go
@@ -18,22 +18,38 @@ func TestSet(t *testing.T) {
 	fmt.Printf("%v\n", *s)
 }
 
-func TestEdgeList(t *testing.T) {
-	g := new(EdgeList)
-	fmt.Printf("edge list: %v\n", g)
+func test(g Graph, t *testing.T) {
+	fmt.Printf("graph: %v\n", g)
 
 	g.AddEdge(1, 2)
 	g.AddEdge(1, 3)
 	g.AddEdge(2, 3)
+	g.AddEdge(3, 4)
 
 	if !g.Adjacent(1, 2) || !g.Adjacent(1, 3) || !g.Adjacent(2, 3) {
 		t.Fail()
 	}
-	fmt.Printf("edge list: %v neighbors of 1: %v\n", g, g.Neighbors(1))
+	fmt.Printf("graph: %v neighbors of 1: %v\n", g, g.Neighbors(1))
 
 	g.RemoveEdge(2, 3)
 	if g.Adjacent(2, 3) {
 		t.Fail()
 	}
-	fmt.Printf("edge list: %v\n", g)
+	fmt.Printf("graph: %v\n", g)
+}
+
+func TestEdgeList(t *testing.T) {
+	g := new(EdgeList)
+
+	fmt.Println("test edge list ...")
+
+	test(g, t)
+}
+
+func TestAdjacencyMatrix(t *testing.T) {
+	g := NewAdjacencyMatrix(5)
+
+	fmt.Println("test adjacency matrix ...")
+
+	test(&g, t)
 }
