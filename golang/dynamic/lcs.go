9dd3e01629192c1345bfbb14aabf9ccab0063f51
Qiang Li <qiang4.li@ge.com>
2019-04-24 12:21:31 -0700

    lcs memoization

--- golang/dynamic/lcs.go
+++ golang/dynamic/lcs.go
@@ -42,3 +42,40 @@ func printLcs(b [][]int, x []byte, i, j int) {
 		printLcs(b, x, i, j-1)
 	}
 }
+
+func lcsfunctor(n int) func([]byte, []byte, [][]int, [][]int, int, int) int {
+	memo := make(map[int][2]int)
+
+	max := func(a, b int) (int, int) {
+		if a >= b {
+			return a, 1
+		}
+		return b, -1
+	}
+	var f func([]byte, []byte, [][]int, [][]int, int, int) int
+	f = func(x, y []byte, c [][]int, b [][]int, i, j int) int {
+		k := n*i + j
+		a, ok := memo[k]
+		if ok {
+			c[i][j] = a[0]
+			b[i][j] = a[1]
+			fmt.Printf("cached: %v %v %v\n", i, j, a)
+			return a[0]
+		}
+		v, di := 0, 0
+		if i == 0 || j == 0 {
+			v = 0
+		} else if x[i] == y[j] {
+			v = f(x, y, c, b, i-1, j-1) + 1
+		} else {
+			v, di = max(f(x, y, c, b, i-1, j), f(x, y, c, b, i, j-1))
+		}
+		c[i][j] = v
+		b[i][j] = di
+
+		fmt.Printf("caching: %v %v %v\n", i, j, a)
+		memo[k] = [2]int{v, di}
+		return v
+	}
+	return f
+}
--- golang/dynamic/lcs_test.go
+++ golang/dynamic/lcs_test.go
@@ -9,20 +9,37 @@ func TestLcs(t *testing.T) {
 	x := []byte{0, 'A', 'B', 'C', 'B', 'D', 'A', 'B'}
 	y := []byte{0, 'B', 'D', 'C', 'A', 'B', 'A'}
 
-	m := len(x)
-	n := len(y)
-	c := make([][]int, m)
-	b := make([][]int, m)
+	//var init func() (int, int, [][]int, [][]int)
+	init := func() (int, int, [][]int, [][]int) {
+		m := len(x)
+		n := len(y)
+		c := make([][]int, m)
+		b := make([][]int, m)
 
-	for i := 0; i < m; i++ {
-		c[i] = make([]int, n)
-		b[i] = make([]int, n)
+		for i := 0; i < m; i++ {
+			c[i] = make([]int, n)
+			b[i] = make([]int, n)
+		}
+		return m, n, c, b
 	}
 
+	m, n, c, b := init()
 	lcs(x, y, c, b, m-1, n-1)
 
 	fmt.Printf("lcs: %v %v\n", x, y)
-	fmt.Printf("lcs: %v\n", c)
+	fmt.Printf("lcs: %v\n%v\n", c, b)
+
+	printLcs(b, x, m-1, n-1)
+	fmt.Println()
+
+	//
+	fmt.Println("lcs functor")
+	m, n, c, b = init()
+	f := lcsfunctor(m)
+	f(x, y, c, b, m-1, n-1)
+
+	fmt.Printf("lcs functor: %v %v\n", x, y)
+	fmt.Printf("lcs functor: %v\n%v\n", c, b)
 
 	printLcs(b, x, m-1, n-1)
 	fmt.Println()
